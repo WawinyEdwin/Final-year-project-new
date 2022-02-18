714ec28e17cffbd730921990a2df9dd112d76fd8
Edwin Odhiambo <wawinyedwin44@gmail.com>
2022-02-18 11:15:25 +0300

    dashboard

--- Agriculture/App.py
+++ Agriculture/App.py
@@ -6,6 +6,8 @@ import MySQLdb.cursors
 import re
 import config
 import requests
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -19,6 +21,11 @@ mysql = MySQL(app)
 
 msg = ""
 suc = ""
+
+
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
 
 # Obtaining weather
 def weather_fetch(city_name):
@@ -246,6 +253,44 @@ def expenses():
 def sales():
     return render_template("sales.html")
 
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Harvestify - Crop Recommendation'
+    return render_template('crop_recommender.html', title=title)
+
+@ app.route('/crop-predict', methods=['POST'])
+
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+@app.route('/admin/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 
--- Agriculture/config.py
+++ Agriculture/config.py
@@ -1,1 +1,34 @@
-API_KEY = "cbb7d92c294d51618c73edfd89f119cd"
+weather_api_key = "cbb7d92c294d51618c73edfd89f119cd"
+
+# import requests
+
+# def weather_fetch(city_name):
+#     """
+#     Fetch and returns the temperature and humidity of a city
+#     :params: city_name
+#     :return: temperature, humidity
+#     """
+#     api_key = weather_api_key
+#     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+#     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+#     response = requests.get(complete_url)
+#     x = response.json()
+
+#     if x["cod"] != "404":
+#         y = x["main"]
+#         r= x["visibility"]
+#         r = x["weather"]
+
+#         temperature = round((y["temp"] - 273.15), 2)
+#         humidity = y["humidity"]
+#         #rain = r["3h"]
+        
+#         print("Temperature ", temperature)
+#         print("Humidity  ", humidity)
+#         print("Humidity  ", r)
+#         return temperature, humidity
+#     else:
+#         return None
+# city = input("Enter city? ")
+# weather_fetch(city)
